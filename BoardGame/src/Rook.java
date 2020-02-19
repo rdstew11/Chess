cb3e3cf2ea12a223ed2d4edd135ae52a7a66b4b3
rdste <rdste@10.80.48.207>
2020-02-19 12:32:30 -0500

    Added castling to rook moveList and removed it after it has moved

--- BoardGame/src/Rook.java
+++ BoardGame/src/Rook.java
@@ -4,6 +4,12 @@ public class Rook extends Piece{
 	public Rook(Team team)
 	{
 		super("rook",team);
+		this.moveList = new String[] {"r","c","castle"};
+	}
+	
+	public void hasMoved(boolean hasMoved)
+	{
+		this.hasMoved = hasMoved;
 		this.moveList = new String[] {"r","c"};
 	}
 }
